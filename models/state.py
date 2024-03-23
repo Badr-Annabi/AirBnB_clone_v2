1c2f1ccfc613635b055c7bfa6484fa7724f983e2
Badr-annabi <badrannabi@gmail.com>
2024-03-23 22:58:54 +0000

    TASK 7

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from models.base_model import Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import sessionmaker, relationship
+import os
 
 
 class State(BaseModel, Base):
@@ -12,15 +13,24 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship(
-        "City", cascade="all, delete-orphan", backref="state")
-
-    @property
-    def cities(self):
-        """
-        returns the list of City instances with state_id
-        equals to the current State.id
-        """
+            "City", cascade="all, delete-orphan", backref="state")
+    type_storage = os.getenv("HBNB_TYPE_STORAGE")
+    if type_storage != 'db':
+        @property
+        def cities(self):
+            """
+            Getter method to return Cities that are linked to States
+            """
+            city_objs = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_obj.append(city)
+            return city_objs
+    """
+        @property
+        def cities(self):
         Session = sessionmaker(bind=Base.metadata.bind)
         with Session() as session:
             cities = session.query(City).filter(City.state_id == self.id).all()
         return cities
+    """
