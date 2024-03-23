2a3556cfbc4841bd54842193f4aad21fcacbcc5c
Badr-annabi <badrannabi@gmail.com>
2024-03-23 05:58:47 +0000

    FIX some bugs

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def display_C(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/', defaults={'is cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text':'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text):
     """ this func displays python + some text """
