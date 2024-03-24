52946df40f9f6cc41a72fd3707a8e78ced86d257
Badr-annabi <badrannabi@gmail.com>
2024-03-24 17:42:03 +0000

    test

--- models/state.py
+++ models/state.py
@@ -14,17 +14,10 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship(
             "City", cascade="all, delete-orphan", backref="state")
-    type_storage = os.getenv("HBNB_TYPE_STORAGE")
-    if type_storage != 'db':
-
+    if not os.getenv('HBNB_TYPE_STORAGE') == "db":
         @property
         def cities(self):
-            """
-            Getter method to return Cities that are linked to States
-            """
             from models import storage
-            city_objs = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    city_objs.append(city)
-            return city_objs
+            cities_list = [city for _, city in storage.all(City).items()
+                           if city.state_id == self.id]
+            return cities_list
